47d3f4dfe74fdf13c30e1db7e05460a539248bf7
hakan yalcin <tchakanyalcin>
2022-12-06 15:26:49 +0300

    first commit

---
+++ async.js
@@ -0,0 +1,23 @@
+const http = require('https');
+
+const start = Date.now();
+
+function doRequest() {
+    http.request('http://www.google.com', res => {
+        res.on('data', () => {});
+        res.on('end', () => {
+            console.log(Date.now() - start);
+        });
+    })
+    .end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
